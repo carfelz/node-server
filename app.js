6da3b3525bd049e14923e8fc1a68bd6a7ecdc09e
Carlos <carlosjfeliz7@gmail.com>
2021-01-30 23:07:33 -0500

    test removed

--- app.js
+++ app.js
@@ -16,7 +16,6 @@ http.createServer((req, res)=>{
     var uri = url.parse(req.url).pathname;
     var fileName = path.join(process.cwd(), unescape(uri));
     console.log(`Loading ${uri}`);
-    console.log(`Did it actually load?`);
 
     var stats;
     try{
